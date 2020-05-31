d10bc83d301b98cea1e2879a00d7ad0a078d5c6c
HoJin9622 <kiss0104040@gmail.com>
2020-05-31 15:14:53 +0900

    코드 정리 3

--- stream-video-browser/webstreaming.py
+++ stream-video-browser/webstreaming.py
@@ -1,4 +1,5 @@
 # 사용법
+# pip install
 # python webstreaming.py
 
 # 필요한 라이브러리를 import
@@ -23,104 +24,119 @@ lock = threading.Lock()
 # Flask object 생성
 app = Flask("__name__")
 
-# initialize the video stream and allow the camera sensor to
-# warmup
-#vs = VideoStream(usePiCamera=1).start()
+# VideoStream을 초기화시키고 카메라 센서가 예열되도록 한다.
+# vs = VideoStream(usePiCamera=1).start()
 vs = VideoStream(src=0).start()
 # 일시 정지 함수
 time.sleep(2.0)
 
+
 @app.route("/")
 def index():
-	# index template 반환
-	return render_template("index.html")
+    # index template 반환
+    return render_template("index.html")
+
 
 # 모션 감지 함수
 def detect_motion(frameCount):
-	# 출력 프레임과 lock, video stream 변수에 대한 전역변수로 참조
-	global vs, outputFrame, lock
+    # 출력 프레임과 lock, video stream 변수에 대한 전역변수로 참조
+    global vs, outputFrame, lock
 
-	# initialize the motion detector and the total number of frames
-	# read thus far
-	md = SingleMotionDetector(accumWeight=0.1)
-	total = 0
+    # initialize the motion detector and the total number of frames
+    # read thus far
+    md = SingleMotionDetector(accumWeight=0.1)
+    total = 0
 
-	# loop over frames from the video stream
-	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    # loop over frames from the video stream
+    while True:
+        # read the next frame from the video stream, resize it,
+        # convert the frame to grayscale, and blur it
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-		# 현재 시간을 구한 후 frame에 그린다.
-		timestamp = datetime.datetime.now()
-		cv2.putText(frame, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        # 현재 시간을 구한 후 frame에 그린다.
+        timestamp = datetime.datetime.now()
+        cv2.putText(
+            frame,
+            timestamp.strftime("%A %d %B %Y %I:%M:%S%p"),
+            (10, frame.shape[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.35,
+            (0, 0, 255),
+            1,
+        )
 
-		# if the total number of frames has reached a sufficient
-		# number to construct a reasonable background model, then
-		# continue to process the frame
-		if total > frameCount:
-			# detect motion in the image
-			motion = md.detect(gray)
+        # if the total number of frames has reached a sufficient
+        # number to construct a reasonable background model, then
+        # continue to process the frame
+        if total > frameCount:
+            # detect motion in the image
+            motion = md.detect(gray)
 
-			# cehck to see if motion was found in the frame
-			if motion is not None:
-				# unpack the tuple and draw the box surrounding the
-				# "motion area" on the output frame
-				(thresh, (minX, minY, maxX, maxY)) = motion
-				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-					(0, 0, 255), 2)
-		
-		# update the background model and increment the total number
-		# of frames read thus far
-		md.update(gray)
-		total += 1
+            # cehck to see if motion was found in the frame
+            if motion is not None:
+                # unpack the tuple and draw the box surrounding the
+                # "motion area" on the output frame
+                (thresh, (minX, minY, maxX, maxY)) = motion
+                cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
 
-		# acquire the lock, set the output frame, and release the
-		# lock
-		with lock:
-			outputFrame = frame.copy()
-		
+        # update the background model and increment the total number
+        # of frames read thus far
+        md.update(gray)
+        total += 1
+
+        # acquire the lock, set the output frame, and release the
+        # lock
+        with lock:
+            outputFrame = frame.copy()
+
+
 def generate():
-	# 출력 프레임과 lock 변수에 대한 전역변수로 참조
-	global outputFrame, lock
+    # 출력 프레임과 lock 변수에 대한 전역변수로 참조
+    global outputFrame, lock
 
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
 
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
 
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
 
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+        # yield the output frame in the byte format
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
+        )
+
 
 @app.route("/video_feed")
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-# check to see if this is the main thread of execution
-if __name__ == '__main__':
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(128,))
-	t.daemon = True
-	t.start()
 
-	# Flask Server 시작
-	app.run(host="0.0.0.0", port="8000", debug=True, threaded=True, use_reloader=False)
+# main thread인지 확인한다.
+if __name__ == "__main__":
+    # detect_motion을 수행할 thread를 시작한다.
+    # daemon thread를 true로 주어 main thread가 종료되면 즉시 종료되게 한다.
+    t = threading.Thread(target=detect_motion, args=(128,))
+    t.daemon = True
+    t.start()
 
-# release the video stream pointer
+    # localhost 8000번 포트에 Flask Server 시작
+    app.run(host="0.0.0.0", port="8000", debug=True, threaded=True, use_reloader=False)
+
+# video stream을 해제한다.
 vs.stop()
